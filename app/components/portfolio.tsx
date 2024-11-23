91d78cd8e9bfc18a9462da7a8a93bac7eaa20602
Nikhil <nexakreation@gmail.com>
2024-11-23 13:16:48 +0530

    old version

--- app/components/portfolio.tsx
+++ app/components/portfolio.tsx
@@ -2,9 +2,9 @@ import React from "react";
 export default function Porfolio() {
     return (
         <nav className="w-full">
-            <ul className="flex items-center justify-between h-8 px-2    bg-black  text-white">
+            <ul className="flex items-center justify-between h-8 px-2 bg-black text-white">
                 <li className="flex items-center">
-                    <a href="https:/ashadeewanexports.com/portfolio" target="_blank" className="text-lg font-bold">Nexakreation</a>
+                    <a href="https:/ashadeewanexports.com/portfolio" target="_blank" className="text-lg font-bold">Portfolio</a>
                 </li>
                 <li className="flex items-center">
                     <a href="https://ashadeewanexports.com/portfolio/web-dev.php" target="_blank" className="ml-4 text-lg px-4 rounded-2xl bg-teal-900">view all projects</a>
